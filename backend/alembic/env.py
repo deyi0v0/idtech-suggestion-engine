55648d7d0cd9519b1ef07ac404141ab5abfa4cf2
Calvin Li <hicalvinli@gmail.com>
2026-02-09 13:36:52 -0800

    feat: alembic setup

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -16,9 +16,12 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+from ..db.base import mymodel
+target_metadata = mymodel.Base.metadata
+
+# Database URL from session file
+from ..db.session import DB_URL
+config.set_main_option("sqlalchemy.url", DB_URL)
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
