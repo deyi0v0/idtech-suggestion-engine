fa6acefa1af52638a58be747c3291e385809a536
Richie Kadota <rkadota@uci.edu>
2026-04-13 22:41:12 -0700

    Product filtering function

--- backend/engine/rulesEngine/product_filtering.py
+++ backend/engine/rulesEngine/product_filtering.py
@@ -1,11 +1,15 @@
 from sqlalchemy.orm import Session
 from typing import Dict, Any, List
-from ...db.repositories.product_query import ProductRepository
+from db.repositories.product_query import ProductRepository
+from engine.rulesEngine.filters_schemas import HardwareFilters
 
-def product_filtering(db: Session, constraints: Dict[str, Any]) -> List[Dict[str, Any]]:
+def product_filtering(db: Session, constraints: HardwareFilters) -> List[Dict[str, Any]]:
     """
     Search product based on constraints like category, use case, or environment.
     Example constraints: {"category": "Outdoor", "use_case": "EV station"}
     """
     repo = ProductRepository(db)
 
+    hardware_options = repo.find_hardware(constraints)
+
+    return hardware_options
