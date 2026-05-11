da3cb85d245bd8662278f19c09170fc45857e99a
gavindg <52291693+gavindg@users.noreply.github.com>
2026-05-11 16:40:25 -0700

    Add IDTECH LOGO to admin portal

--- frontend/src/pages/maintenance/AdminLayout.tsx
+++ frontend/src/pages/maintenance/AdminLayout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router";
 import "./admin.css";
+import IDTechLogoBlack from "./components/IDTechLogoBlack";
 
 export default function AdminLayout() {
     return (
@@ -21,7 +22,12 @@ export default function AdminLayout() {
 function DashboardNavbar() {
     return (
         <nav className="flex justify-between pl-5 m-0 pr-5 bg-white w-full min-h-16 items-end border-b-2 shrink-0">
-            <h1 className="font-semibold text-4xl">Admin Portal</h1>
+            <div className="flex items-end">
+                <div className="pb-2">
+                    <IDTechLogoBlack />
+                </div>
+                <h1 className="italic font-semibold text-4xl p-0 pl-3">Admin Portal</h1>
+            </div>
             <div className="flex justify-between">
                 <a href="/admin/hardware" className="pr-2 text-gray-600 text-xl hover:text-gray-900">
                     Hardware
@@ -35,11 +41,12 @@ function DashboardNavbar() {
 }
 
 function ChatbotPlaceholder() {
-    return (
-        <div className="flex flex-col bg-amber-200 h-full min-w-[400px] p-[10px] items-center justify-center">
-            <h1 className="bg-red-100 max-w-fit max-h-fit p-[10px]">
-                chatbot component
-            </h1>
-        </div>
-    );
+    return <></>;
+    // return (
+    //     <div className="flex flex-col bg-amber-200 h-full min-w-[400px] p-[10px] items-center justify-center">
+    //         <h1 className="bg-red-100 max-w-fit max-h-fit p-[10px]">
+    //             chatbot component
+    //         </h1>
+    //     </div>
+    // );
 }
