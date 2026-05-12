92fbd49b27139997ee0ccba8f9de5fdda72dc6e5
gavindg <52291693+gavindg@users.noreply.github.com>
2026-05-11 22:22:44 -0700

    Add placeholder test

---
+++ tests/backend/placeholder_test.py
@@ -0,0 +1,5 @@
+# placeholder test to avoid false negative while no tests have been written for the backend yet.
+# TODO: remove this file when backend tests are written.
+
+def test_placeholder():
+    assert True
